bed4cc6f2ef29e0d368659e6534f7ea703a88802
Administrator <Administrator@DACPC38.actspnq.cdac.in>
2016-04-18 20:31:09 +0530

    updated new class for testing

--- src/Day2/Assignment/point2ds.java
+++ src/Day2/Assignment/point2ds.java
@@ -11,8 +11,15 @@ package Day2.Assignment;
  */
 public class point2ds {
     private double x,y;
-
-    public point2ds() {
+    private point2ds obj;
+    
+    public point2ds getInstance() {
+        if(obj==null){
+            obj = new point2ds();
+        }
+        return obj;
+    }
+    private point2ds(){
         x = 0.0;
         y = 0.0;
     }
@@ -23,8 +30,24 @@ public class point2ds {
     public void show(){
         System.out.println("cordinator x = "+x + " and y = "+y);
    }
+    public boolean equals(point2ds obj){
+        if(obj.x == x && obj.y == y){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
     public static void main(String[] args) {
         point2ds p = new point2ds(10, 20);
+        point2ds p2 = new point2ds(10,20);
+        if(p.equals(p2)){
+            System.out.println("Equal");
+        }else{
+            System.out.println("Not Equal");
+        }
         p.show();
+        p2.show();
     }
+    
 }
